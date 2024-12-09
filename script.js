80e2993a71eb1f606f8ca44b8d73ae935fdda11b
Ofer Guez <guez.ofer@gmail.com>
2024-12-09 16:19:41 +0000

    1

--- script.js
+++ script.js
@@ -19,10 +19,51 @@ function ResetAnimation()
     controller.reset();
 }
 
+const colors = [
+    "Red",
+    "Blue",
+    "Green",
+    "Yellow",
+    "Purple",
+    "Orange",
+    "Pink",
+    "Brown",
+    "Cyan",
+    "Magenta"
+  ];
+ 
 
-function SayHi()
+
+function setRandomFwStyle(fwStyle)
+{ 
+    let size = Math.floor(Math.random() * 70) + 30;
+    fwStyle.width = `${size+1}px`;
+    fwStyle.height = `${size+1}px`;
+  
+    let randomIndex = Math.floor(Math.random() * colors.length);
+    randomColor = colors[randomIndex].toLowerCase();
+    let randomTransparency = Math.floor(40 + 20*Math.random());
+  
+    fwStyle.background = `radial-gradient(circle, ${randomColor} ${100-randomTransparency}%, transparent ${randomTransparency}%)`;
+}
+
+function GetGoing()
 {
-    console.log('Hi');
+    const currentFireworks = document.getElementsByClassName('Child');
+    Array.from(currentFireworks).forEach(fw => fw.remove());
+
+    const inputField = document.getElementById('numberInput');
+    const fwNumber = parseFloat(inputField.value);
+    const parent = document.getElementById('Parent');
+    for (let fw = 0; fw < fwNumber; fw++)
+    {
+        const firework = document.createElement('div');
+        let ChildClass = 1 + fw%4;
+        firework.classList.add('Child', `Child${ChildClass}`);
+        firework.id = `Child${fw}`;
+        setRandomFwStyle(firework.style);
+        parent.appendChild(firework);
+    }
 }
 
 
