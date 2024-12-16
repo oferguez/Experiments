7ffa20796aa5bbcaf84ed065beee4786035db34a
Ofer Guez <guez.ofer@gmail.com>
2024-12-16 16:24:29 +0000

    1

--- script.js
+++ script.js
@@ -37,14 +37,11 @@ function setAnimationProperties()
 {
         // Common animation options for all elements
     const commonOptions = {
-        // duration: 5000,
-        // iterations: 1,
         easing: 'ease-in-out',
         fill: 'forwards'
     };
 
     animationLen = 5000;
-
 
     // Child1 - bottom-right animation
     const childs1 = Array.from(document.querySelectorAll('.Child1'));
@@ -71,7 +68,7 @@ function setAnimationProperties()
     const childs2  = Array.from(document.querySelectorAll('.Child2'));
     childs2.forEach( child2 => {
         setRandomFwStyle(child2);
-        child2.style.transform = 'translate(-100%, -100%)';
+        //child2.style.transform = 'translate(-100%, -100%)';
         randomRepeats = 5 + Math.floor(5*Math.random()); // 5..9
         child2.animate([
             { transform: 'translate(-100%, -100%) scale(1)', opacity: 1 },
@@ -93,7 +90,7 @@ function setAnimationProperties()
     const childs3  = Array.from(document.querySelectorAll('.Child3'));
     childs3.forEach( child3 => {
         setRandomFwStyle(child3);
-        child3.style.transform = 'translate(0, -100%)';
+        //child3.style.transform = 'translate(0, -100%)';
         randomRepeats = 5 + Math.floor(5*Math.random()); // 5..9
         child3.animate([
             { transform: 'translate(0, -100%) scale(1)', opacity: 1 },
@@ -115,7 +112,7 @@ function setAnimationProperties()
     const childs4  = Array.from(document.querySelectorAll('.Child4'));
     childs4.forEach( child4 => {
         setRandomFwStyle(child4);
-        child4.style.transform = 'translate(-100%, 0)';
+        //child4.style.transform = 'translate(-100%, 0)';
         randomRepeats = 5 + Math.floor(5*Math.random()); // 5..9
         child4.animate([
             { transform: 'translate(-100%, 0) scale(1)', opacity: 1 },
@@ -150,7 +147,13 @@ function setRandomFwStyle(fw)
     fwStyle.background = `radial-gradient(circle, ${randomColor} ${100-randomTransparency}%, transparent ${randomTransparency}%)`;
 }
 
-function GetGoing()
+function GetGoing() {
+    const parent = document.getElementById('Parent');
+    let fwExtravaganza = new  FireworkExtravaganza(parent);
+    fwExtravaganza.fireAll();
+}
+
+function GetGoing_old()
 {
     const currentFireworks = document.getElementsByClassName('Child');
     Array.from(currentFireworks).forEach(fw => fw.remove());
@@ -191,4 +194,98 @@ class AnimationController {
         this.animations.forEach( a => a.cancel() );
         this.animations.forEach( a => a.play() );
     }
+}
+
+class FireworkExtravaganza {
+    constructor(parentElement) {
+        this.parent = parentElement;
+        let inputField = document.getElementById('numFireworks');
+        this.fwNumber = parseFloat(inputField.value);
+        this.commonOptions = {
+            easing: 'ease-in-out',
+            fill: 'forwards'
+        };
+        let lengthField =  document.getElementById('Length');
+        this.animationLen = 1000 * parseFloat(lengthField.value);
+
+        this.parentWidth = parentElement.clientWidth;
+        this.parentHeight = parentElement.clientHeight;
+        console.log('Constructed');
+    }
+    fireAll() {
+        console.log('firing all...');
+        this.cleanup();
+        for ( let fw = 0; fw < this.fwNumber; fw++ ) {
+           this.fire(fw);
+        }
+        console.log('fired all');
+    }
+    cleanup() {
+        const currentFireworks = document.getElementsByClassName('Child');
+        Array.from(currentFireworks).forEach(fw => fw.remove());
+    }
+    fire(fw) {
+        const firework = document.createElement('div');
+        firework.classList.add('Child');
+        firework.id = `Child${fw}`;
+        this.parent.appendChild(firework);
+    
+        let positioning = this.setRandomFwStyle(firework);
+        this.setRandomFwAnimation(firework, positioning);
+    }
+
+    setRandomFwAnimation(fw, postioning) {
+        let randomRepeats = 5 + Math.floor(5*Math.random()); // 5..9       
+        let midOpcacity = 0.2 + Math.random()  / 2; // 0.2..0.7 
+        let midScale = 0.5 + Math.random() * 2; // 0.5..2.5
+        let midOffset = 0.3 + 0.4 * Math.random(); // 0.3..0.7
+        let dx = postioning.endPos.x - postioning.startPos.x;
+        let dy = postioning.endPos.y - postioning.startPos.y;
+        fw.animate([
+            { transform: 'scale(1)', opacity: 1 },
+            { transform: `translate(${midOffset*dx}px, ${midOffset*dy}px) scale(${midScale})`, opacity: midOpcacity, offset: midOffset },
+            { 
+                transform: `translate(${dx}px, ${dy}px) scale(1)`,
+                opacity: 0,
+            }
+        ], {
+            ...this.commonOptions,
+            iterations: randomRepeats,
+            duration: this.animationLen / randomRepeats
+        });  
+    }
+
+    setRandomFwStyle(fw)
+    { 
+        let fwStyle = fw.style;
+
+        let size1 = Math.floor(Math.random() * 70) + 60;
+        let positioning = {
+            startPos: this.getRandomConfinedPos(size1),
+            endPos: this.getRandomConfinedPos(size1),
+            size: size1
+        }
+
+        fwStyle.width = `${positioning.size+1}px`;
+        fwStyle.height = `${positioning.size+1}px`; 
+        let randomIndex = Math.floor(Math.random() * colors.length);
+        let randomColor = colors[randomIndex].toLowerCase();
+        let randomTransparency = Math.floor(40 + 20*Math.random());
+        
+        fwStyle.background = `radial-gradient(circle, ${randomColor} ${100-randomTransparency}%, transparent ${randomTransparency}%)`;
+
+
+        fwStyle.left = `${positioning.startPos.x}px`;
+        fwStyle.top = `${positioning.startPos.y}px`;
+
+        return positioning;
+    }
+
+    getRandomConfinedPos(size)
+    {
+        const x = Math.floor(Math.random() * (this.parentWidth - size));
+        const y = Math.floor(Math.random() * (this.parentHeight - size));
+        return {x,y};
+    }
+
 }
