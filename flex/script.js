4c721c958c1a47bfa7c961b9be82bd2ad9b0b003
Ofer Guez <guez.ofer@gmail.com>
2025-01-20 16:01:52 +0000

    1

--- flex/script.js
+++ flex/script.js
@@ -6,14 +6,19 @@ function sliderInput()
     sizeLabel.textContent = size;
     const container = document.querySelector('.container');
     container.style.setProperty('--size', size)
+    console.log(`sliderInput: new size: ${size}`);
     render();
 }
 
 function render()
 {
+    console.log(`render called`);
+
     const container = document.querySelector('.container');
     container.innerHTML = ''; // clearup 
     container.style.setProperty('--size', size); // set css variable
+
+    console.log(`render starting loop`);
 
     for (let i = 0; i < size; i++) {
         const row = document.createElement('div');
@@ -24,10 +29,14 @@ function render()
             box.className = 'box';
             box.id = boxName;
             box.textContent = boxName;
+            box.addEventListener('click', () => {alert(`Box ${boxName} clicked!`);});
             row.appendChild(box);
         }
         container.appendChild(row);
     }
+
+    console.log(`render finished loop`);
+
 }
 
 sliderInput();
